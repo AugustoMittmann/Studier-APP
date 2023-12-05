0e133dd0b8f887a4df2009a6a768b1f41c643bf3
Augusto Mittmann <gutomittmann10@gmail.com>
2023-12-04 22:33:11 -0300

    header, botao login e modal login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,58 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap'
+import { Button, Modal, Form } from 'react-bootstrap'
 import './css.css';
+import { useState } from 'react';
+//import mysql from 'mysql2';
 
 function Header() {
+  const[loginModal, setLoginModal] = useState();
+
+  const handleSubmit = () => {
+    const usuario = document.getElementById('formBasicUser');
+    const senha = document.getElementById('formBasicPassword');
+    console.log(usuario.value);
+    console.log(senha.value)
+  };
 
   return (
     <>
-      <Container fluid className='header'>
-        <h1>Studier</h1>
-      </Container>
+      <div className='header'>
+          <div class="headerName">
+            <h1>Studier</h1>
+          </div>
+          <div class="headerButton">
+            <Button variant="light" onClick={() => setLoginModal(true)}>Login</Button>{' '}
+          </div>
+      </div>
+      { loginModal ? 
+      <Modal show={loginModal} onHide={() => setLoginModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login / Registrar-se</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicUser" >
+              <Form.Label>Usuário</Form.Label>
+              <Form.Control type="string" placeholder="Coloque seu usuário" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword" >
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Coloque sua senha" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmit}>Login</Button>
+          <Button variant="primary">Criar conta</Button>
+        </Modal.Footer>
+      </Modal>
+      : <></>
+      }
     </>
   );
 }
